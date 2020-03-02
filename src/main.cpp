9d330ff7a3d2ffddfa823ebcc0bac52fcbac3977
DAMEK86 <rehn.andreas86@gmail.com>
2020-03-02 21:12:48 +0100

    split html to avoid heap fragmentation, add json endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ ESP8266WebServer server(80);
 
 Edl21 encoder;
 
-
 extern const char* ssid;
 extern const char* password;
 
@@ -33,6 +32,24 @@ float currentconsumptionHTkWh, currentconsumptionNTkWh, deliveryHTkWh, deliveryN
 int powerAll, powerL1, powerL2, powerL3;
 int smlIndex; //index counter within smlMessage array
 int state = 0;
+
+const char* HEADER = "<!DOCTYPE html> <html>\n\
+  <head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n\
+  <META http-equiv=\"refresh\" content=\"10\">\n\
+  <title>Smart-Meter</title>\n\
+  <style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n\
+  body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-top: 50px;}\n\
+  .button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 14px;margin: 25px auto 35px;cursor: pointer;border-radius: 4px;}\n\
+  .button-on {background-color: #1abc9c;}\n\
+  .button-on:active {background-color: #16a085;}\n\
+  .button-off {background-color: #34495e;}\n\
+  .button-off:active {background-color: #2c3e50;}\n\
+  p {font-size: 14px;color: #888;margin-bottom: 10px;}\n\
+  </style>\n\
+  </head>\n";
+
+const char* FOOTER = "</body>\n \
+  </html>\n";
 
 void findStopSequence() {
   while (Serial.available())
@@ -85,36 +102,26 @@ float findConsumptionInSequence(unsigned char* sequence, unsigned char sequenceS
   return float(currentconsumption)/10000; // 10.000 impulses per kWh
 }
 
-void array_to_string(byte array[], unsigned int len, char buffer[])
-{
-    for (unsigned int i = 0; i < len; i++)
-    {
-        byte nib1 = (array[i] >> 4) & 0x0F;
-        byte nib2 = (array[i] >> 0) & 0x0F;
-        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
-    }
-    buffer[len*2] = '\0';
+void getJson() {
+  String json = String("{\
+  \"1.8.0\":\"") + String(consumptionOverallkWh) + String("\",\
+  \"1.8.1\":\"") + String(currentconsumptionHTkWh) + String("\",\
+  \"1.8.2\":\"") + String(currentconsumptionNTkWh) + String("\",\
+  \"2.8.0\":\"") + String(deliveredOverallkWh) + String("\",\
+  \"2.8.1\":\"") + String(deliveryHTkWh) + String("\",\
+  \"2.8.2\":\"") + String(deliveryNTkWh) + String("\",\
+  \"16.7.0\":\"") + String(powerAll) + String("\",\
+  \"36.7.0\":\"") + String(powerL1) + String("\",\
+  \"56.7.0\":\"") + String(powerL2) + String("\",\
+  \"76.7.0\":\"") + String(powerL3) + String("\"\
+  }");
+  server.send(200,"text/html", json);
 }
 
-String SendHTML(){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<META http-equiv=\"refresh\" content=\"10\">\n";
-  ptr +="<title>Smart-Meter</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-top: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 14px;margin: 25px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #1abc9c;}\n";
-  ptr +=".button-on:active {background-color: #16a085;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
+String getBody(){
+  String ptr ="<body>\n";
   ptr +="<h1>ESP8266 Smart-Meter Web Server</h1>\n";
-  
+
   ptr +="<h3>Verbrauch (A+)</h3>\n";
   ptr +="<p>Gesamt  - 1.8.0: " + String(consumptionOverallkWh) + " kWh</p>";
   ptr +="<p>Tarif 1 - 1.8.1: " + String(currentconsumptionHTkWh) + " kWh</p>";
@@ -131,6 +138,11 @@ String SendHTML(){
   ptr +="<p>Phase L2 - 56.7.0: " + String(powerL2) + " W</p>";
   ptr +="<p>Phase L3 - 76.7.0: " + String(powerL3) + " W</p>";
 
+  return ptr;
+}
+
+String GetRawMessage() {
+  String ptr;
   if (showSmlMessage) {
     ptr +="<a class=\"button button-off\" href=\"/hide\">Hide RAW</a>\n";
     ptr +="<p>SML Message: ";
@@ -149,13 +161,15 @@ String SendHTML(){
     ptr +="<a class=\"button button-off\" href=\"/show\">Show RAW</a>\n";
   }
 
-  ptr +="</body>\n";
-  ptr +="</html>\n";
   return ptr;
 }
 
 void handle_OnConnect() {
-  server.send(200, "text/html", SendHTML()); 
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200,"text/html", HEADER);
+  server.sendContent(getBody());
+  server.sendContent(GetRawMessage());
+  server.sendContent(FOOTER);
 }
 
 void handle_OnHide() {
@@ -237,6 +251,7 @@ void setup() {
   digitalWrite(led, 1);
 
   server.on("/", handle_OnConnect);
+  server.on("/data", getJson);
   server.on("/hide", handle_OnHide);
   server.on("/show", handle_OnShow);
   server.onNotFound(handleNotFound);
